a627d3647fb42adbd1e3dcedc526a42a0b5e51ba
Thibault APRETNA <thibaultapretna@Thibaults-MacBook-Air.local>
2024-10-30 18:03:25 +0100

    Troisième Commit

--- routes/cart.js
+++ routes/cart.js
@@ -4,4 +4,12 @@ router.use(express.json());
 
 const Cart = require("../models/Cart");
 
+router.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
